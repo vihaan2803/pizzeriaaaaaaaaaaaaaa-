9e67f4106e31db82378da35fdd48822a4b7e61b5
vihaan2803 <74404558+vihaan2803@users.noreply.github.com>
2021-02-25 19:10:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+menu_list_array = ["Chicken Tandoori Pizza", "Veg Supreme Pizza", "Paneer Tikka Pizza", "Deluxe Veggie Pizza", "Veg Extravaganza Pizza"];
+
+function getmenu() {
+    var htmldata;
+    htmldata = "<ol class='menulist'>"
+    menu_list_array.sort();
+    for (var i = 0; i < menu_list_array.length; i++) {
+        htmldata = htmldata + '<li>' + menu_list_array[i] + '</li>'
+    }
+    htmldata = htmldata + "</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
